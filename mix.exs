682dfb6b22e82b72a7bb4f0ba51440e63ccac82c
Indrek Juhkam <indrek@urgas.eu>
2018-10-10 22:38:11 +0300

    Add ex_doc to dev dependencies for documentation generation

--- mix.exs
+++ mix.exs
@@ -33,8 +33,8 @@ defmodule OpenTracing.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      # Build documentation (run `mix docs`)
+      {:ex_doc, "~> 0.18.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
