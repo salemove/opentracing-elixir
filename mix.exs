3b62561880643c53e5e28ae4970e0266943e6e80
Indrek Juhkam <indrek@urgas.eu>
2018-10-10 22:34:32 +0300

    Add package information to mix.exs
    These can be later changed if the lib is actually added to OpenTracing.

--- mix.exs
+++ mix.exs
@@ -6,8 +6,20 @@ defmodule OpenTracing.MixProject do
       app: :opentracing,
       version: "0.1.0",
       elixir: "~> 1.6",
+      description: "OpenTracing API for Elixir",
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
+    ]
+  end
+
+  def package do
+    [
+      maintainers: ["SaleMove TechMovers"],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/salemove/opentracing-elixir"
+      }
     ]
   end
 
